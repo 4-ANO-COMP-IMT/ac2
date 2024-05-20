06b24bde6780f54baf1af18f65d974fdf3427a66
Enzo Sakamoto <enzo.sak@hotmail.com>
2024-05-19 22:07:12 -0300

    feat(get-event): add stage inversion

--- back/event/src/modules/get_event/get_event_presenter.ts
+++ back/event/src/modules/get_event/get_event_presenter.ts
@@ -3,6 +3,10 @@ import { GetEventController } from './get_event_controller'
 import { EventRepositoryMock } from '../../shared/infra/repos/event_repository_mock'
 import { EventRepositoryInterface } from '../../shared/infra/repos/event_repository_interface'
 import { GetEventRequest, GetEventResponse } from './protocols'
+import { EventRepositoryHttp } from '../../shared/infra/repos/event_repository_http'
+import { config } from 'dotenv'
+
+config()
 
 export interface GetEventPresenterProps {
   repo: EventRepositoryInterface
@@ -11,13 +15,16 @@ export interface GetEventPresenterProps {
   call(req: GetEventRequest): Promise<GetEventResponse>
 }
 
+const stage = process.env.STAGE || 'test'
+
 export class GetEventPresenter implements GetEventPresenterProps {
   repo: EventRepositoryInterface
   usecase: GetEventUsecase
   controller: GetEventController
 
   constructor() {
-    this.repo = new EventRepositoryMock()
+    this.repo =
+      stage === 'test' ? new EventRepositoryMock() : new EventRepositoryHttp()
     this.usecase = new GetEventUsecase(this.repo)
     this.controller = new GetEventController(this.usecase)
   }
