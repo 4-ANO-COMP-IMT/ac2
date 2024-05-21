4d74e6e76a7f0755e1a74a02c2015d3dd3ff4db8
Enzo Sakamoto <enzo.sak@hotmail.com>
2024-05-20 21:11:18 -0300

    refactor(get-event): simplify GetEventRequest structure

--- back/src/event/modules/get_event/protocols.ts
+++ back/src/event/modules/get_event/protocols.ts
@@ -1,25 +1,11 @@
 import { EventJsonProps } from '../../../shared/domain/entities/event'
 
 export type GetEventRequest = {
-  params: {
-    id: string
-  }
+  id: string
 }
 
 export type GetEventResponse = {
   status: number
   message: string
   event?: EventJsonProps
-}
-
-export type httpRequest<T = any, V = any> = {
-  method: 'get' | 'post' | 'put' | 'delete'
-  params?: T
-  body?: V
-}
-
-export type httpResponse<T = any> = {
-  status: number
-  message: string
-  data?: T
 }
