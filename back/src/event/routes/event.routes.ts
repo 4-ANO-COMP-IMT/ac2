c836a9865252b5e3f6ded35ffc85948d92e798a8
Enzo Sakamoto <enzo.sak@hotmail.com>
2024-05-20 23:50:50 -0300

    feat(get-event): add new request object

--- back/src/event/routes/event.routes.ts
+++ back/src/event/routes/event.routes.ts
@@ -5,8 +5,9 @@ import { HttpRequest } from '../../shared/domain/helpers/http/http_request'
 export const eventRouter = Router()
 
 eventRouter.get('/:id', async (req, res) => {
+  const request = new HttpRequest('get', req.params)
   const presenter = new GetEventPresenter()
-  const response = await presenter.call(new HttpRequest('get', req.params))
+  const response = await presenter.call(request)
   res
     .status(response.status)
     .json({ message: response.message, data: response.data })
