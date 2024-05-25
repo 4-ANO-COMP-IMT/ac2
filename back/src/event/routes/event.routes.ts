21c13cd8107b42bb806218037a08ad4af97c2d49
Enzo Sakamoto <enzo.sak@hotmail.com>
2024-05-25 09:15:36 -0300

    feat(post-event): add type for body request

--- back/src/event/routes/event.routes.ts
+++ back/src/event/routes/event.routes.ts
@@ -14,12 +14,25 @@ eventRouter.get('/:id', async (req, res) => {
     .json({ message: response.message, data: response.data })
 })
 
-eventRouter.post('/', async (req, res) => {
-  const request = new HttpRequest('post', req.body)
-  const presenter = new PostEventPresenter()
-  const response = await presenter.call(request)
+eventRouter.post(
+  '/',
+  async (
+    req: {
+      body: {
+        name: string
+        startDate: number
+        endDate: number
+        timeInterval: number
+      }
+    },
+    res
+  ) => {
+    const request = new HttpRequest('post', req.body)
+    const presenter = new PostEventPresenter()
+    const response = await presenter.call(request)
 
-  res
-    .status(response.status)
-    .json({ message: response.message, data: response.data })
-})
+    res
+      .status(response.status)
+      .json({ message: response.message, data: response.data })
+  }
+)
