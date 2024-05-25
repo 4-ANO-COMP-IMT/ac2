256629ba90363f234cddde4391e21789139bcb54
Enzo Sakamoto <enzo.sak@hotmail.com>
2024-05-25 09:15:58 -0300

    refactor(repo-interface): refat createEvent method

--- back/src/event/shared/infra/repos/event_repository_interface.ts
+++ back/src/event/shared/infra/repos/event_repository_interface.ts
@@ -3,6 +3,11 @@ import { Event } from '../../../../shared/domain/entities/event'
 export interface EventRepositoryInterface {
   getEvent(id: string): Promise<Event>
   putEvent(event: Event): Promise<Event>
-  createEvent(event: Event): Promise<Event>
+  createEvent(event: {
+    name: string
+    startDate: number
+    endDate: number
+    timeInterval: number
+  }): Promise<Event>
   deleteEvent(id: string): Promise<Event>
 }
