f8d1a6f44ed4e514fe2be7c13eda73003790ba73
Enzo Sakamoto <enzo.sak@hotmail.com>
2024-05-25 08:30:55 -0300

    feat(event): add delete event

--- back/src/dynamo/index.ts
+++ back/src/dynamo/index.ts
@@ -10,6 +10,23 @@ type Event = {
   timeInterval: number
 }
 
+const users: Event[] = [
+  {
+    id: '1',
+    name: 'LEG DAY WITHOUT REST',
+    startDate: 1632856800000,
+    endDate: 1632856800000,
+    timeInterval: 60
+  },
+  {
+    id: '2',
+    name: 'ESQUEMA PIRAMIDE DO BRANCAS',
+    startDate: 1632856800000,
+    endDate: 1632856800000,
+    timeInterval: 60
+  }
+]
+
 const server = async () => {
   config()
   const app = express()
@@ -26,28 +43,27 @@ const server = async () => {
   app.get('/event/:id', (req, res) => {
     const id = req.params.id
 
-    const users: Event[] = [
-      {
-        id: '1',
-        name: 'LEG DAY WITHOUT REST',
-        startDate: 1632856800000,
-        endDate: 1632856800000,
-        timeInterval: 60
-      },
-      {
-        id: '2',
-        name: 'ESQUEMA PIRAMIDE DO BRANCAS',
-        startDate: 1632856800000,
-        endDate: 1632856800000,
-        timeInterval: 60
-      }
-    ]
-
     const event = users.find((event) => event.id === id)
 
     if (!event) {
       res.status(404).json({ message: 'Event not found' })
     }
+
+    res.status(200).json(event)
+  })
+
+  app.delete('/event/:id', (req, res) => {
+    const id = req.params.id
+
+    const eventIndex = users.findIndex((event) => event.id === id)
+
+    if (eventIndex === -1) {
+      res.status(404).json({ message: 'Event not found' })
+    }
+
+    const event = users[eventIndex]
+
+    users.splice(eventIndex, 1)
 
     res.status(200).json(event)
   })
