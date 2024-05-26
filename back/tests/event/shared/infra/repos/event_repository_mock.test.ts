d07b291612b5a03e0a003a6231f8dab035d4490e
Pedro Matumoto <pedromatumoto@gmail.com>
2024-05-25 21:25:10 -0300

    feat(test): add tests to refat in put

--- back/tests/event/shared/infra/repos/event_repository_mock.test.ts
+++ back/tests/event/shared/infra/repos/event_repository_mock.test.ts
@@ -24,42 +24,359 @@ test('Test get event not found', () => {
   }).rejects.toThrowError('event not found')
 })
 
-test('Test put event found', async () => {
+test('Test put name', async () => {
   const repo = new EventRepositoryMock()
-  const event = await repo.putEvent(
-    new Event(
-      '3',
-      'Studying Software Engineering',
-      1632950000000,
-      1632954000000,
-      100000
-    )
-  )
+  const event = await repo.putEvent('2', {
+    name: 'Academy Leg Day'
+  })
   const eventExpect = new Event(
-    '3',
-    'Studying Software Engineering',
-    1632950000000,
+    '2',
+    'Academy Leg Day',
+    1632950200000,
     1632954000000,
-    100000
+    300000
   )
   expect(event).not.toBe(undefined)
   expect(event).toBeInstanceOf(Event)
+  expect(event?.name).toBe('Academy Leg Day')
+  expect(event?.startDate).toBe(1632950200000)
+  expect(event?.endDate).toBe(1632954000000)
+  expect(event?.timeInterval).toBe(300000)
   expect(event?.equals(eventExpect)).toBe(true)
+  repo.resetMock()
+})
+
+test('Test put start date', async () => {
+  const repo = new EventRepositoryMock()
+  const event = await repo.putEvent('2', {
+    start_date: 1632950000000
+  })
+  const eventExpect = new Event(
+    '2',
+    'Academy Chest Day',
+    1632950000000,
+    1632954000000,
+    300000
+  )
+  expect(event).not.toBe(undefined)
+  expect(event).toBeInstanceOf(Event)
+  expect(event?.name).toBe('Academy Chest Day')
+  expect(event?.startDate).toBe(1632950000000)
+  expect(event?.endDate).toBe(1632954000000)
+  expect(event?.timeInterval).toBe(300000)
+  expect(event?.equals(eventExpect)).toBe(true)
+  repo.resetMock()
+})
+
+test('Test put end date', async () => {
+  const repo = new EventRepositoryMock()
+  const event = await repo.putEvent('2', {
+    end_date: 1632954000000
+  })
+  const eventExpect = new Event(
+    '2',
+    'Academy Chest Day',
+    1632950200000,
+    1632954000000,
+    300000
+  )
+  expect(event).not.toBe(undefined)
+  expect(event).toBeInstanceOf(Event)
+  expect(event?.name).toBe('Academy Chest Day')
+  expect(event?.startDate).toBe(1632950200000)
+  expect(event?.endDate).toBe(1632954000000)
+  expect(event?.timeInterval).toBe(300000)
+  expect(event?.equals(eventExpect)).toBe(true)
+  repo.resetMock()
+})
+
+test('Test put time interval', async () => {
+  const repo = new EventRepositoryMock()
+  const event = await repo.putEvent('2', {
+    time_interval: 600000
+  })
+  const eventExpect = new Event(
+    '2',
+    'Academy Chest Day',
+    1632950200000,
+    1632954000000,
+    600000
+  )
+  expect(event).not.toBe(undefined)
+  expect(event).toBeInstanceOf(Event)
+  expect(event?.name).toBe('Academy Chest Day')
+  expect(event?.startDate).toBe(1632950200000)
+  expect(event?.endDate).toBe(1632954000000)
+  expect(event?.timeInterval).toBe(600000)
+  expect(event?.equals(eventExpect)).toBe(true)
+  repo.resetMock()
+})
+
+test('Test put event name and start date', async () => {
+  const repo = new EventRepositoryMock()
+  const event = await repo.putEvent('2', {
+    name: 'Academy Leg Day',
+    start_date: 1632950000000
+  })
+  const eventExpect = new Event(
+    '2',
+    'Academy Leg Day',
+    1632950000000,
+    1632954000000,
+    300000
+  )
+  expect(event).not.toBe(undefined)
+  expect(event).toBeInstanceOf(Event)
+  expect(event?.name).toBe('Academy Leg Day')
+  expect(event?.startDate).toBe(1632950000000)
+  expect(event?.endDate).toBe(1632954000000)
+  expect(event?.timeInterval).toBe(300000)
+  expect(event?.equals(eventExpect)).toBe(true)
+  repo.resetMock()
+})
+
+test('Test put event name and end date', async () => {
+  const repo = new EventRepositoryMock()
+  const event = await repo.putEvent('2', {
+    name: 'Academy Leg Day',
+    end_date: 1632954000000
+  })
+  const eventExpect = new Event(
+    '2',
+    'Academy Leg Day',
+    1632950200000,
+    1632954000000,
+    300000
+  )
+  expect(event).not.toBe(undefined)
+  expect(event).toBeInstanceOf(Event)
+  expect(event?.name).toBe('Academy Leg Day')
+  expect(event?.startDate).toBe(1632950200000)
+  expect(event?.endDate).toBe(1632954000000)
+  expect(event?.timeInterval).toBe(300000)
+  expect(event?.equals(eventExpect)).toBe(true)
+  repo.resetMock()
+})
+
+test('Test put event name and time interval', async () => {
+  const repo = new EventRepositoryMock()
+  const event = await repo.putEvent('2', {
+    name: 'Academy Leg Day',
+    time_interval: 600000
+  })
+  const eventExpect = new Event(
+    '2',
+    'Academy Leg Day',
+    1632950200000,
+    1632954000000,
+    600000
+  )
+  expect(event).not.toBe(undefined)
+  expect(event).toBeInstanceOf(Event)
+  expect(event?.name).toBe('Academy Leg Day')
+  expect(event?.startDate).toBe(1632950200000)
+  expect(event?.endDate).toBe(1632954000000)
+  expect(event?.timeInterval).toBe(600000)
+  expect(event?.equals(eventExpect)).toBe(true)
+  repo.resetMock()
+})
+
+test('Test put event start date and end date', async () => {
+  const repo = new EventRepositoryMock()
+  const event = await repo.putEvent('2', {
+    start_date: 1632950000000,
+    end_date: 1632954000000
+  })
+  const eventExpect = new Event(
+    '2',
+    'Academy Chest Day',
+    1632950000000,
+    1632954000000,
+    300000
+  )
+  expect(event).not.toBe(undefined)
+  expect(event).toBeInstanceOf(Event)
+  expect(event?.name).toBe('Academy Chest Day')
+  expect(event?.startDate).toBe(1632950000000)
+  expect(event?.endDate).toBe(1632954000000)
+  expect(event?.timeInterval).toBe(300000)
+  expect(event?.equals(eventExpect)).toBe(true)
+  repo.resetMock()
+})
+
+test('Test put event start date and time interval', async () => {
+  const repo = new EventRepositoryMock()
+  const event = await repo.putEvent('2', {
+    start_date: 1632950000000,
+    time_interval: 600000
+  })
+  const eventExpect = new Event(
+    '2',
+    'Academy Chest Day',
+    1632950000000,
+    1632954000000,
+    600000
+  )
+  expect(event).not.toBe(undefined)
+  expect(event).toBeInstanceOf(Event)
+  expect(event?.name).toBe('Academy Chest Day')
+  expect(event?.startDate).toBe(1632950000000)
+  expect(event?.endDate).toBe(1632954000000)
+  expect(event?.timeInterval).toBe(600000)
+  expect(event?.equals(eventExpect)).toBe(true)
+  repo.resetMock()
+})
+
+test('Test put event end date and time interval', async () => {
+  const repo = new EventRepositoryMock()
+  const event = await repo.putEvent('2', {
+    end_date: 1632954000000,
+    time_interval: 600000
+  })
+  const eventExpect = new Event(
+    '2',
+    'Academy Chest Day',
+    1632950200000,
+    1632954000000,
+    600000
+  )
+  expect(event).not.toBe(undefined)
+  expect(event).toBeInstanceOf(Event)
+  expect(event?.name).toBe('Academy Chest Day')
+  expect(event?.startDate).toBe(1632950200000)
+  expect(event?.endDate).toBe(1632954000000)
+  expect(event?.timeInterval).toBe(600000)
+  expect(event?.equals(eventExpect)).toBe(true)
+  repo.resetMock()
+})
+
+test('Test put event name, start date and end date', async () => {
+  const repo = new EventRepositoryMock()
+  const event = await repo.putEvent('2', {
+    name: 'Academy Leg Day',
+    start_date: 1632950000000,
+    end_date: 1632954000000
+  })
+  const eventExpect = new Event(
+    '2',
+    'Academy Leg Day',
+    1632950000000,
+    1632954000000,
+    300000
+  )
+  expect(event).not.toBe(undefined)
+  expect(event).toBeInstanceOf(Event)
+  expect(event?.name).toBe('Academy Leg Day')
+  expect(event?.startDate).toBe(1632950000000)
+  expect(event?.endDate).toBe(1632954000000)
+  expect(event?.timeInterval).toBe(300000)
+  expect(event?.equals(eventExpect)).toBe(true)
+  repo.resetMock()
+})
+
+test('Test put event name, start date and time interval', async () => {
+  const repo = new EventRepositoryMock()
+  const event = await repo.putEvent('2', {
+    name: 'Academy Leg Day',
+    start_date: 1632950000000,
+    time_interval: 600000
+  })
+  const eventExpect = new Event(
+    '2',
+    'Academy Leg Day',
+    1632950000000,
+    1632954000000,
+    600000
+  )
+  expect(event).not.toBe(undefined)
+  expect(event).toBeInstanceOf(Event)
+  expect(event?.name).toBe('Academy Leg Day')
+  expect(event?.startDate).toBe(1632950000000)
+  expect(event?.endDate).toBe(1632954000000)
+  expect(event?.timeInterval).toBe(600000)
+  expect(event?.equals(eventExpect)).toBe(true)
+  repo.resetMock()
+})
+
+test('Test put event name, end date and time interval', async () => {
+  const repo = new EventRepositoryMock()
+  const event = await repo.putEvent('2', {
+    name: 'Academy Leg Day',
+    end_date: 1632954000000,
+    time_interval: 600000
+  })
+  const eventExpect = new Event(
+    '2',
+    'Academy Leg Day',
+    1632950200000,
+    1632954000000,
+    600000
+  )
+  expect(event).not.toBe(undefined)
+  expect(event).toBeInstanceOf(Event)
+  expect(event?.name).toBe('Academy Leg Day')
+  expect(event?.startDate).toBe(1632950200000)
+  expect(event?.endDate).toBe(1632954000000)
+  expect(event?.timeInterval).toBe(600000)
+  expect(event?.equals(eventExpect)).toBe(true)
+  repo.resetMock()
+})
+
+test('Test put event start date, end date and time interval', async () => {
+  const repo = new EventRepositoryMock()
+  const event = await repo.putEvent('2', {
+    start_date: 1632950000000,
+    end_date: 1632954000000,
+    time_interval: 600000
+  })
+  const eventExpect = new Event(
+    '2',
+    'Academy Chest Day',
+    1632950000000,
+    1632954000000,
+    600000
+  )
+  expect(event).not.toBe(undefined)
+  expect(event).toBeInstanceOf(Event)
+  expect(event?.name).toBe('Academy Chest Day')
+  expect(event?.startDate).toBe(1632950000000)
+  expect(event?.endDate).toBe(1632954000000)
+  expect(event?.timeInterval).toBe(600000)
+  expect(event?.equals(eventExpect)).toBe(true)
+  repo.resetMock()
+})
+
+test('Test put event name, start date, end date and time interval', async () => {
+  const repo = new EventRepositoryMock()
+  const event = await repo.putEvent('2', {
+    name: 'Academy Leg Day',
+    start_date: 1632950000000,
+    end_date: 1632954000000,
+    time_interval: 600000
+  })
+  const eventExpect = new Event(
+    '2',
+    'Academy Leg Day',
+    1632950000000,
+    1632954000000,
+    600000
+  )
+  expect(event).not.toBe(undefined)
+  expect(event).toBeInstanceOf(Event)
+  expect(event?.name).toBe('Academy Leg Day')
+  expect(event?.startDate).toBe(1632950000000)
+  expect(event?.endDate).toBe(1632954000000)
+  expect(event?.timeInterval).toBe(600000)
+  expect(event?.equals(eventExpect)).toBe(true)
+  repo.resetMock()
 })
 
 test('Test put event not found', async () => {
   const repo = new EventRepositoryMock()
   expect(async () => {
-    await repo.putEvent(
-      new Event(
-        '1001',
-        'Studying Software Engineering',
-        1632950000000,
-        1632954000000,
-        100000
-      )
-    )
+    await repo.putEvent('1001', {})
   }).rejects.toThrowError('event not found')
+  repo.resetMock()
 })
 
 test('Test create event', () => {
