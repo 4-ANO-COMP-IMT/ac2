33b5c2632523c4dea746aadeb4e704873da940e7
Pedro Matumoto <pedromatumoto@gmail.com>
2024-05-25 21:27:22 -0300

    deat(put): add put presenter

---
+++ back/src/event/modules/put_event/put_event_presenter.ts
@@ -0,0 +1,43 @@
+import { PutEventUsecase } from './put_event_usecase'
+import { PutEventController } from './put_event_controller'
+import { EventRepositoryMock } from '../../shared/infra/repos/event_repository_mock'
+import { EventRepositoryInterface } from '../../shared/infra/repos/event_repository_interface'
+import { PutEventRequest } from './protocols'
+import { EventRepositoryHttp } from '../../shared/infra/repos/event_repository_http'
+import { config } from 'dotenv'
+import { HttpRequest } from '../../../shared/domain/helpers/http/http_request'
+import {
+  Error,
+  HttpResponse
+} from '../../../shared/domain/helpers/http/http_response'
+import { EventJsonProps } from '../../../shared/domain/entities/event'
+
+config()
+
+export interface PutEventPresenterProps {
+  repo: EventRepositoryInterface
+  usecase: PutEventUsecase
+  controller: PutEventController
+  call(
+    req: HttpRequest<PutEventRequest>
+  ): Promise<HttpResponse<EventJsonProps> | HttpResponse<Error>>
+}
+
+const stage = process.env.STAGE || 'test'
+
+export class PutEventPresenter implements PutEventPresenterProps {
+  repo: EventRepositoryInterface
+  usecase: PutEventUsecase
+  controller: PutEventController
+
+  constructor() {
+    this.repo =
+      stage === 'test' ? new EventRepositoryMock() : new EventRepositoryHttp()
+    this.usecase = new PutEventUsecase(this.repo)
+    this.controller = new PutEventController(this.usecase)
+  }
+
+  async call(req: HttpRequest<PutEventRequest>) {
+    return await this.controller.call(req)
+  }
+}
