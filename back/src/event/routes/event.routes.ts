97dfc905aabe4b463399c39b40e1cd3ad97ba282
Enzo Sakamoto <enzo.sak@hotmail.com>
2024-05-26 14:37:18 -0300

    feat(event): add put event route

--- back/src/event/routes/event.routes.ts
+++ back/src/event/routes/event.routes.ts
@@ -2,12 +2,22 @@ import { Router } from 'express'
 import { GetEventPresenter } from '../modules/get_event/get_event_presenter'
 import { HttpRequest } from '../../shared/domain/helpers/http/http_request'
 import { DeleteEventPresenter } from '../modules/delete_event/delete_event_presenter'
+import { PutEventPresenter } from '../modules/put_event/put_event_presenter'
 
 export const eventRouter = Router()
 
 eventRouter.get('/:id', async (req, res) => {
   const request = new HttpRequest('get', req.params)
   const presenter = new GetEventPresenter()
+  const response = await presenter.call(request)
+  res
+    .status(response.status)
+    .json({ message: response.message, data: response.data })
+})
+
+eventRouter.put('/:id', async (req, res) => {
+  const request = new HttpRequest('put', Object.assign(req.params, req.body))
+  const presenter = new PutEventPresenter()
   const response = await presenter.call(request)
   res
     .status(response.status)
