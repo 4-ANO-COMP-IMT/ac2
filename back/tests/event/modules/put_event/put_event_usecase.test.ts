980ace352408bce16ac96f0a7ae716bdcbb9ac78
Pedro Matumoto <pedromatumoto@gmail.com>
2024-05-25 21:25:47 -0300

    feat(test): add test to usecase

---
+++ back/tests/event/modules/put_event/put_event_usecase.test.ts
@@ -0,0 +1,25 @@
+import { EventRepositoryMock } from '../../../../src/event/shared/infra/repos/event_repository_mock'
+import { PutEventUsecase } from '../../../../src/event/modules/put_event/put_event_usecase'
+import { Event } from '../../../../src/shared/domain/entities/event'
+
+import { test, expect } from 'vitest'
+
+test('Test put event name given', async () => {
+  const repo = new EventRepositoryMock()
+  const usecase = new PutEventUsecase(repo)
+  const event = await usecase.call('2', 'Academy Leg Day')
+  expect(event).toBeInstanceOf(Event)
+  expect(event?.id).toBe('2')
+  expect(event?.name).toBe('Academy Leg Day')
+  expect(event?.startDate).toBe(1632950200000)
+  expect(event?.endDate).toBe(1632954000000)
+  expect(event?.timeInterval).toBe(300000)
+})
+
+test('Test get event usecase not found', () => {
+  const repo = new EventRepositoryMock()
+  const usecase = new PutEventUsecase(repo)
+  expect(async () => {
+    await usecase.call('1001')
+  }).rejects.toThrowError('event not found')
+})
