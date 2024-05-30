0066990b0ddae343419b05f18ad44009d3dbfdc0
Vitor Soller <vgsoller@gmail.com>
2024-05-30 19:37:35 -0300

    refat(schedule-mss): add schedule json props on schedule entity

--- back/src/shared/domain/entities/schedule.ts
+++ back/src/shared/domain/entities/schedule.ts
@@ -1,78 +1,73 @@
-export interface ScheduleInterface {
-    
-}
+export interface ScheduleInterface {}
 
 export type ScheduleJsonProps = {
-
+  id: string
+  event_id: string
+  time: number
+  name: string
 }
 
 export class Schedule implements ScheduleInterface {
-    private _id: string
-    private _eventId: string
-    private _time: number
-    private _name: string
+  private _id: string
+  private _eventId: string
+  private _time: number
+  private _name: string
 
-    constructor(
-        id: string,
-        eventId: string,
-        time: number,
-        name: string
-    ) {
-        this._id = id
-        this._eventId = eventId
-        this._time = time
-        this._name = name
-    }
+  constructor(id: string, eventId: string, time: number, name: string) {
+    this._id = id
+    this._eventId = eventId
+    this._time = time
+    this._name = name
+  }
 
-    equal(schedule: Schedule) {
-        return (
-            this._id === schedule._id &&
-            this._eventId === schedule._eventId &&
-            this._time === schedule._time &&
-            this._name === schedule._name
-        )
-    }
+  equal(schedule: Schedule) {
+    return (
+      this._id === schedule._id &&
+      this._eventId === schedule._eventId &&
+      this._time === schedule._time &&
+      this._name === schedule._name
+    )
+  }
 
-    toJson() {
-        return {
-            id: this._id,
-            event_id: this._eventId,
-            time: this._time,
-            name: this._name
-        }
+  toJson() {
+    return {
+      id: this._id,
+      event_id: this._eventId,
+      time: this._time,
+      name: this._name
     }
+  }
 
-    // Getters and Setters
-    get id() {
-        return this._id
-    }
+  // Getters and Setters
+  get id() {
+    return this._id
+  }
 
-    get eventId() {
-        return this._eventId
-    }
+  get eventId() {
+    return this._eventId
+  }
 
-    get time() {
-        return this._time
-    }
+  get time() {
+    return this._time
+  }
 
-    get name() {
-        return this._name
-    }
+  get name() {
+    return this._name
+  }
 
-    set id(id: string) {
-        this._id = id
-    }
+  set id(id: string) {
+    this._id = id
+  }
 
-    set eventId(eventId: string) {
-        this._eventId = eventId
-    }
+  set eventId(eventId: string) {
+    this._eventId = eventId
+  }
 
-    set time(time: number) {
-        this._time = time
-    }
+  set time(time: number) {
+    this._time = time
+  }
 
-    set name(name: string) {
-        this._name = name
-    }
-    
+  set name(name: string) {
+    this._name = name
+  }
 }
