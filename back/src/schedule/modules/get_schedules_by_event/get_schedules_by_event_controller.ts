160f0b1dca6565876565dfa6853afe45078d4488
Vitor Soller <vgsoller@gmail.com>
2024-05-30 19:57:38 -0300

    feat(schedule-mss): add get schedules by event controller without UT

---
+++ back/src/schedule/modules/get_schedules_by_event/get_schedules_by_event_controller.ts
@@ -0,0 +1,45 @@
+import {
+  Error,
+  HttpResponse
+} from '../../../shared/domain/helpers/http/http_response'
+
+import { HttpRequest } from './../../../shared/domain/helpers/http/http_request'
+import { ScheduleJsonProps } from '../../../shared/domain/entities/schedule'
+import { GetSchedulesByEventUsecase } from './get_schedules_by_event_usecase'
+import { GetSchedulesByEventRequest } from './protocols'
+
+interface GetSchedulesByEventControlerProps {
+  usecase: GetSchedulesByEventUsecase
+  call(
+    req: HttpRequest<GetSchedulesByEventRequest>
+  ): Promise<HttpResponse<ScheduleJsonProps> | HttpResponse<Error>>
+}
+
+export class GetEventController implements GetSchedulesByEventControlerProps {
+  constructor(public usecase: GetSchedulesByEventUsecase) {
+    this.usecase = usecase
+  }
+
+  async call(req: HttpRequest<GetSchedulesByEventRequest>) {
+    const eventId = req.data?.eventId
+
+    if (!eventId) {
+      return HttpResponse.badRequest('eventId is required')
+    }
+
+    try {
+      const schedules = await this.usecase.call(eventId)
+
+      return HttpResponse.ok<ScheduleJsonProps[]>(
+        'schedules found',
+        schedules.map((schedule) => schedule.toJson())
+      )
+    } catch (error: any) {
+      if (error.message.includes('not found')) {
+        return HttpResponse.notFound(error.message)
+      }
+
+      return HttpResponse.internalServerError(error.message)
+    }
+  }
+}
