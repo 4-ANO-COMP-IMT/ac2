a5a9e22e9496c86fc6c1379601bdee409c13dd1d
Vitor Soller <vgsoller@gmail.com>
2024-05-30 19:21:48 -0300

    feat(schedule-mss): add schedule repository

---
+++ back/src/schedule/shared/infra/repos/schedule_repository_interface.ts
@@ -0,0 +1,12 @@
+import { Schedule } from '../../../../shared/domain/entities/schedule'
+
+export interface ScheduleRepositoryInterface {
+  getSchedulesByEventId(eventId: string): Promise<Schedule[]>
+  createSchedule(schedule: {
+    eventId: string
+    time: number
+    name: string
+  }): Promise<Schedule>
+  deleteSchedule(scheduleId: string): Promise<Schedule>
+  // Batch operations?
+}
