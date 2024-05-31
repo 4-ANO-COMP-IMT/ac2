4b1807430062cb10ae27796ea0b5bdfea866c68a
Flavio Murata <flaviomurata02@gmail.com>
2024-05-30 21:50:52 -0300

    test(schedule-mss): add get schedule event by id type unit test

--- back/tests/schedule/modules/get_schedules_by_event/get_schedules_by_event_presenter.test.ts
+++ back/tests/schedule/modules/get_schedules_by_event/get_schedules_by_event_presenter.test.ts
@@ -1,20 +1,15 @@
 import { test, expect } from 'vitest'
 import { GetSchedulesByEventPresenter } from '../../../../src/schedule/modules/get_schedules_by_event/get_schedules_by_event_presenter'
 import { HttpRequest } from '../../../../src/shared/domain/helpers/http/http_request'
+import { HTTP_STATUS_CODE } from '../../../../src/shared/domain/helpers/http/http_status_code'
 
-test('Test get event presenter found', async () => {
+test('Test get schedule by event presenter returns an object', async () => {
   const presenter = new GetSchedulesByEventPresenter()
   const response = await presenter.call(
     new HttpRequest('get', { eventId: '2' })
   )
-  const eventExpect = {
-    id: '2',
-    name: 'Academy Chest Day',
-    start_date: 1632950200000,
-    end_date: 1632954000000,
-    time_interval: 300000
-  }
-  expect(response.status).toBe(200)
-  expect(response.message).toBe('schedule found')
-  expect(response.data).toEqual(eventExpect)
+
+  expect(response.status).toBe(HTTP_STATUS_CODE.OK)
+  expect(response.message).toBe('schedules found')
+  expect(response.data).toBeTypeOf('object')
 })
