517bcd46ce3eea4ff2ff7de4d672e52fc7ac427f
Flavio Murata <flaviomurata02@gmail.com>
2024-05-31 14:52:13 -0300

    fix(schedule-repo): modify the not found error message

--- back/src/schedule/shared/infra/repos/schedule_repository_http.ts
+++ back/src/schedule/shared/infra/repos/schedule_repository_http.ts
@@ -1,13 +1,15 @@
-import { Event } from '../../../../shared/domain/entities/event'
 import { Schedule } from '../../../../shared/domain/entities/schedule'
-import { NotFoundError } from '../../../../shared/domain/helpers/errors/not_found'
 import { ScheduleRepositoryInterface } from './schedule_repository_interface'
 
 export class ScheduleRepositoryHttp implements ScheduleRepositoryInterface {
   getSchedulesByEventId(eventId: string): Promise<Schedule[]> {
     throw new Error('Method not implemented.')
   }
-  createSchedule(schedule: { eventId: string; time: number; name: string }): Promise<Schedule> {
+  createSchedule(schedule: {
+    eventId: string
+    time: number
+    name: string
+  }): Promise<Schedule> {
     throw new Error('Method not implemented.')
   }
   deleteSchedule(scheduleId: string): Promise<Schedule> {
