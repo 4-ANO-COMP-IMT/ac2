d148bc49664f03e081198be3f58f8904ea7fa5cc
Flavio Murata <flaviomurata02@gmail.com>
2024-05-31 14:53:09 -0300

    feat(schedule-route): add delete schedule route

--- back/src/schedule/routes/schedule.routes.ts
+++ back/src/schedule/routes/schedule.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express'
 
 import { HttpRequest } from '../../shared/domain/helpers/http/http_request'
+import { DeleteSchedulePresenter } from '../modules/delete_schedules_by_event/delete_schedules_presenter'
 import { GetSchedulesByEventPresenter } from '../modules/get_schedules_by_event/get_schedules_by_event_presenter'
 
 export const scheduleRouter = Router()
@@ -8,6 +9,15 @@ export const scheduleRouter = Router()
 scheduleRouter.get('/eventId=:eventId', async (req, res) => {
   const request = new HttpRequest('get', req.params)
   const presenter = new GetSchedulesByEventPresenter()
+  const response = await presenter.call(request)
+  res
+    .status(response.status)
+    .json({ message: response.message, data: response.data })
+})
+
+scheduleRouter.delete('/scheduleId=:id', async (req, res) => {
+  const request = new HttpRequest('delete', req.params)
+  const presenter = new DeleteSchedulePresenter()
   const response = await presenter.call(request)
   res
     .status(response.status)
