4fe85b4c6d1411267271a473b1308763edcb9376
Flavio Murata <flaviomurata02@gmail.com>
2024-05-30 22:01:36 -0300

    feat(schedule-mss): add get schedule by event route

--- back/src/schedule/routes/schedule.routes.ts
+++ back/src/schedule/routes/schedule.routes.ts
@@ -1,5 +1,15 @@
 import { Router } from 'express'
 
 import { HttpRequest } from '../../shared/domain/helpers/http/http_request'
+import { GetSchedulesByEventPresenter } from '../modules/get_schedules_by_event/get_schedules_by_event_presenter'
 
 export const scheduleRouter = Router()
+
+scheduleRouter.get('/eventId=:eventId', async (req, res) => {
+  const request = new HttpRequest('get', req.params)
+  const presenter = new GetSchedulesByEventPresenter()
+  const response = await presenter.call(request)
+  res
+    .status(response.status)
+    .json({ message: response.message, data: response.data })
+})
