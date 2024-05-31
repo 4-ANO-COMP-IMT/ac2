759e063de4a24e507642d3051bfdb5d937bc214c
Flavio Murata <flaviomurata02@gmail.com>
2024-05-30 21:41:53 -0300

    test(schedule-mss): add unit test verifying the returned data

--- back/tests/schedule/modules/get_schedules_by_event/get_schedules_by_event_controller.test.ts
+++ back/tests/schedule/modules/get_schedules_by_event/get_schedules_by_event_controller.test.ts
@@ -5,12 +5,12 @@ import { HTTP_STATUS_CODE } from '../../../../src/shared/domain/helpers/http/htt
 import { HttpRequest } from '../../../../src/shared/domain/helpers/http/http_request'
 import { ScheduleRepositoryMock } from '../../../../src/schedule/shared/infra/repos/schedule_repository_mock'
 import { GetSchedulesByEventUsecase } from '../../../../src/schedule/modules/get_schedules_by_event/get_schedules_by_event_usecase'
-import { GetEventController } from '../../../../src/schedule/modules/get_schedules_by_event/get_schedules_by_event_controller'
+import { GetSchedulesByEventController } from '../../../../src/schedule/modules/get_schedules_by_event/get_schedules_by_event_controller'
 
 test('Test get schedule by event controller without eventId', async () => {
   const repo = new ScheduleRepositoryMock()
   const usecase = new GetSchedulesByEventUsecase(repo)
-  const controller = new GetEventController(usecase)
+  const controller = new GetSchedulesByEventController(usecase)
   const request = new HttpRequest('get', { eventId: '' })
   const response = await controller.call(request)
   expect(response.status).toBe(HTTP_STATUS_CODE.BAD_REQUEST)
@@ -18,13 +18,20 @@ test('Test get schedule by event controller without eventId', async () => {
   expect(response.data).toBe(undefined)
 })
 
-// test('Test get event controller without id', async () => {
-//   const repo = new EventRepositoryMock()
-//   const usecase = new GetEventUsecase(repo)
-//   const controller = new GetEventController(usecase)
-//   const request = new HttpRequest('get', { id: '' })
-//   const response = await controller.call(request)
-//   expect(response.status).toBe(HTTP_STATUS_CODE.BAD_REQUEST)
-//   expect(response.message).toBe('id is required')
-//   expect(response.data).toBe(undefined)
-// })
+test('Test get schedule by event controller returns an array', async () => {
+  const repo = new ScheduleRepositoryMock()
+  const usecase = new GetSchedulesByEventUsecase(repo)
+  const controller = new GetSchedulesByEventController(usecase)
+  const request = new HttpRequest('get', { eventId: '2' })
+  const response = await controller.call(request)
+  const scheduleExpect = {
+    id: '2',
+    event_id: '2',
+    start_date: 1632950200000,
+    end_date: 1632954000000,
+    time_interval: 300000
+  }
+  expect(response.status).toBe(HTTP_STATUS_CODE.OK)
+  expect(response.message).toBe('schedules found')
+  expect(response.data).toBeTypeOf('object')
+})
