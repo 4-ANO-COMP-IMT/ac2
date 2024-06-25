f5411430fd68cdd6d39f6461ed47811777ade174
Enzo Sakamoto <enzo.sak@hotmail.com>
2024-06-25 20:26:55 -0300

    feat(home): add home component

---
+++ front/src/components/Home.tsx
@@ -0,0 +1,7 @@
+export function Home() {
+  return (
+    <main className="flex h-[120rem] w-full flex-col items-center justify-center font-geist">
+      <h1 className="">Add colors. Make it yours.</h1>
+    </main>
+  )
+}
