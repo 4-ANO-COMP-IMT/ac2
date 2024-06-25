0bf43a47b3d6191099550dfc3f7772b66603d4f7
Enzo Sakamoto <enzo.sak@hotmail.com>
2024-06-25 20:25:31 -0300

    chore(fonts): add core fonts

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,9 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import Unfonts from 'unplugin-fonts/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    Unfonts({
+      custom: {
+        families: [
+          {
+            name: 'Geist',
+            src: './src/assets/fonts/geist/*.woff2'
+          }
+        ]
+      }
+    })
+  ],
   server: {
     port: 5000
   }
