6d58e682a4d36a4853673ace825cd5eb3a4baece
Enzo Sakamoto <enzo.sak@hotmail.com>
2024-06-25 20:25:59 -0300

    feat(theme): add theme colors

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        primary: '#284C63',
+        secondary: '#34607F',
+        tertiary: '#f2f0f0'
+      }
+    },
+    fontFamily: {
+      geist: ['Geist', 'sans-serif']
+    }
   },
   plugins: []
 }
