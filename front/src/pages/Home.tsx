b57cb684a8e593e4cfab65b225efaca0f061640d
Enzo Sakamoto <enzo.sak@hotmail.com>
2024-06-27 13:19:43 -0300

    fix(layout): remove event card component

--- front/src/pages/Home.tsx
+++ front/src/pages/Home.tsx
@@ -1,9 +1,7 @@
-import { EventCard } from '@/components/EventCard'
-
 export function Home() {
   return (
     <main className="flex h-[120rem] w-full flex-col items-center justify-center font-geist">
-      <EventCard />
+      colors
     </main>
   )
 }
