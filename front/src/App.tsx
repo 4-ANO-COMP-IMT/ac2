57359df1e3ae665dac44886c4d49654864c6ae93
Enzo Sakamoto <enzo.sak@hotmail.com>
2024-06-27 13:20:03 -0300

    feat(theme): add theme provider

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,17 +1,22 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Navbar } from '@/components/Navbar'
-import { Home } from '@/components/Home'
+import { Home } from '@/pages/Home'
+import { Event } from '@/pages/Event'
+import { ThemeProvider } from './contexts/theme_context'
 
 export default function App() {
   return (
     <>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="*" element={<Home />} />
-        </Routes>
-      </Router>
+      <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/event" element={<Event />} />
+            <Route path="*" element={<Home />} />
+          </Routes>
+        </Router>
+      </ThemeProvider>
     </>
   )
 }
