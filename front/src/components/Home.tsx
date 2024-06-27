fd49c9e2d3e0d99bfc21395a98863ec00ff5e630
Enzo Sakamoto <enzo.sak@hotmail.com>
2024-06-26 21:38:05 -0300

    feat(component): add event card component

--- front/src/components/Home.tsx
+++ front/src/components/Home.tsx
@@ -1,7 +1,9 @@
+import { EventCard } from '@/components/EventCard'
+
 export function Home() {
   return (
     <main className="flex h-[120rem] w-full flex-col items-center justify-center font-geist">
-      <h1 className="">Add colors. Make it yours.</h1>
+      <EventCard />
     </main>
   )
 }
