e8d4e46b16bed2483693a4ffa5e12296235c7392
Enzo Sakamoto <enzo.sak@hotmail.com>
2024-06-28 13:45:38 -0300

    fix(animation): change circle animation

--- front/src/pages/Event.tsx
+++ front/src/pages/Event.tsx
@@ -16,9 +16,9 @@ export function Event() {
         className={`absolute grid h-full w-full grid-cols-2 justify-items-center gap-12 transition-all duration-1000 ${fade ? 'opacity-100' : 'opacity-0'}`}
       >
         <div className="h-[320px] w-[320px] animate-one self-end justify-self-end rounded-full bg-blue-900 opacity-50 blur-[7rem]"></div>
-        <div className="h-[320px] w-[320px] animate-one rounded-full  bg-blue-500 opacity-50 blur-[7rem]"></div>
+        <div className="h-[320px] w-[320px] animate-two rounded-full  bg-blue-500 opacity-50 blur-[7rem]"></div>
         <div className="h-[320px] w-[320px] animate-two rounded-full  bg-blue-300 opacity-50 blur-[7rem]"></div>
-        <div className="h-[320px] w-[320px] animate-two self-start justify-self-start rounded-full  bg-blue-600 opacity-50 blur-[7rem]"></div>
+        <div className="h-[320px] w-[320px] animate-one self-start justify-self-start rounded-full  bg-blue-600 opacity-50 blur-[7rem]"></div>
       </div>
       <div
         className={`transition-all duration-500 ${fade ? 'translate-x-0 opacity-100' : 'translate-x-28 opacity-0'}`}
