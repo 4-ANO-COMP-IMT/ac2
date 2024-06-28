d1061bf32e6ab11e3de4ad04e5506e1fc042ec95
Enzo Sakamoto <enzo.sak@hotmail.com>
2024-06-28 13:45:14 -0300

    refactor(lint): fix all linting erros

--- front/src/components/ReviewCard.tsx
+++ front/src/components/ReviewCard.tsx
@@ -4,15 +4,19 @@ import { IoSunnyOutline } from 'react-icons/io5'
 import { RiMenu2Fill } from 'react-icons/ri'
 import { IoClose } from 'react-icons/io5'
 
-export function ReviewCard(props: { img: string, review: string, name: string, position: string }) {
-  
+export function ReviewCard(props: {
+  img: string
+  review: string
+  name: string
+  position: string
+}) {
   return (
-    <div className='flex flex-col justify-center items-center w-80 h-auto gap-2 bg-blue-secondary rounded-2xl px-6 py-6 shadow-blue-primary shadow-lg'> 
+    <div className="flex h-auto w-80 flex-col items-center justify-center gap-2 rounded-2xl bg-blue-secondary px-6 py-6 shadow-lg shadow-blue-primary">
       <img src={props.img} alt="Bruno Vilardi" className="w-2/5 rounded-full" />
-      <p className='text-wrap text-white'>{props.review}</p>
+      <p className="text-wrap text-white">{props.review}</p>
       <div>
-        <p className='text-black'>{props.name}</p>
-        <p className='text-blue-500'>{props.position}</p>
+        <p className="text-black">{props.name}</p>
+        <p className="text-blue-500">{props.position}</p>
       </div>
     </div>
   )
