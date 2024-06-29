959f36a6c2eb21e69402d74bc600ad08eb6a71e2
Enzo Sakamoto <enzo.sak@hotmail.com>
2024-06-29 15:17:47 -0300

    refactor(hooks): rename use_theme.ts to use-theme.ts

--- front/src/hooks/use-theme.ts
+++ front/src/hooks/use-theme.ts
@@ -1,4 +1,4 @@
-import { ThemeProviderContext } from '@/contexts/theme_context'
+import { ThemeProviderContext } from '@/contexts/theme-context'
 import { useContext } from 'react'
 
 export const useTheme = () => {
