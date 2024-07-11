fc8effb30e204f58cf90266a0fa4c0028ec1cd2e
Enzo Sakamoto <enzo.sak@hotmail.com>
2024-07-10 23:22:19 -0300

    feat(component): add first period selector look

--- front/src/components/period-selector.tsx
+++ front/src/components/period-selector.tsx
@@ -1,13 +1,100 @@
 import { Card } from '@/components/ui/card'
+import { useState } from 'react'
 
-export function PeriodSelector({ dates }: { dates: Date[] }) {
+type PeriodSelectorProps = {
+  dates: Date[]
+  notEarlierThan: number
+  notLaterThan: number
+  timezone: number
+}
+
+type Interval = {
+  startDate: Date
+  endDate: Date
+}
+
+export function PeriodSelector({
+  dates,
+  notEarlierThan,
+  notLaterThan,
+  timezone
+}: PeriodSelectorProps) {
+  const intervals: Interval[] = dates
+    .sort((a, b) => a.getTime() - b.getTime())
+    .map((date) => {
+      const startDate = new Date(date)
+      startDate.setHours(notEarlierThan)
+      const endDate = new Date(date)
+      endDate.setHours(notLaterThan)
+      return {
+        // Add timezone offset
+        startDate: new Date(startDate.getTime() + timezone * 60 * 60 * 1000),
+        endDate: new Date(endDate.getTime() + timezone * 60 * 60 * 1000)
+      }
+    })
+
+  const [isDragging, setIsDragging] = useState(false)
+  const [paintedDivs, setPaintedDivs] = useState<{
+    [id: number]: number[]
+  }>({})
+
+  const handleMouseUp = () => {
+    setIsDragging(false)
+  }
+
+  const handleMouseDown = () => {
+    setIsDragging(true)
+  }
+
+  const handleMouseOver = (id: number, index: number) => {
+    if (isDragging) {
+      setPaintedDivs((prev) => {
+        const currentIndices = prev[id] || []
+        const isIndexPainted = currentIndices.includes(index)
+
+        return {
+          ...prev,
+          [id]: isIndexPainted
+            ? currentIndices.filter((i) => i !== index) // Remove o index se já estiver pintado
+            : [...currentIndices, index] // Adiciona o index se não estiver pintado
+        }
+      })
+    }
+  }
+
   return (
-    <Card className="flex gap-[2px] p-2">
-      {dates.map((date, index) => (
+    <Card className="flex gap-[1px] p-2" onMouseUp={handleMouseUp}>
+      {intervals.map((interval, id) => (
         <div
-          key={index}
-          className="flex h-[500px] w-[150px] flex-col items-center justify-center rounded-sm border-[1px] border-primary/30 bg-primary/10"
-        ></div>
+          key={id}
+          className="flex w-24 flex-col items-center justify-center"
+        >
+          <p>{interval.startDate.toLocaleDateString()}</p>
+          <div
+            onMouseUp={handleMouseUp}
+            onMouseDown={handleMouseDown}
+            className="flex w-24 select-none flex-col items-center justify-center rounded-sm border-[1px] border-primary/30"
+          >
+            {Array.from({
+              length:
+                ((interval.endDate.getHours() - interval.startDate.getHours()) *
+                  60) /
+                15
+            }).map((_, index) => (
+              <div
+                key={index}
+                onMouseOver={() => handleMouseOver(id, index)}
+                className={`flex h-[13px] w-full border-primary/30 ${paintedDivs[id] && paintedDivs[id].includes(index) ? 'bg-blue-300/60' : 'bg-transparent'} ${
+                  index === 0
+                    ? 'border-t-0'
+                    : index % 2 === 0
+                      ? 'border-t-[1px] border-solid'
+                      : 'border-t-[1px] border-dashed'
+                }`}
+              ></div>
+            ))}
+          </div>
+        </div>
       ))}
     </Card>
   )
--- front/src/pages/Event.tsx
+++ front/src/pages/Event.tsx
@@ -1,9 +1,8 @@
 import { PeriodSelector } from '@/components/period-selector'
-import { useParams } from 'react-router-dom'
+// import { useParams } from 'react-router-dom'
 
 export function Event() {
-  const { id } = useParams()
-  console.log(id)
+  // const { id } = useParams()
   const mockDates: Date[] = [
     '2024-07-12T03:00:00.000Z',
     '2024-07-11T03:00:00.000Z',
@@ -14,11 +13,14 @@ export function Event() {
     '2024-07-15T03:00:00.000Z'
   ].map((date) => new Date(date))
 
-  console.log(mockDates[0].toISOString())
-
   return (
     <main className="flex h-screen w-full flex-col items-center justify-center">
-      <PeriodSelector dates={mockDates} />
+      <PeriodSelector
+        dates={mockDates}
+        notEarlierThan={8}
+        notLaterThan={18}
+        timezone={-3}
+      />
     </main>
   )
 }
