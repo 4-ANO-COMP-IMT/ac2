b2243ebe6603d0f9e78c363d47415232d9284386
Enzo Sakamoto <enzo.sak@hotmail.com>
2024-07-12 23:52:33 -0300

    feat(context): Add EventProvider for managing event context

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -5,21 +5,24 @@ import { CreateEvent } from '@/pages/CreateEvent'
 import { Event } from '@/pages/Event'
 import { ThemeProvider } from '@/contexts/theme-context'
 import { Toaster } from '@/components/ui/toaster'
+import { EventProvider } from './contexts/event-context.tsx'
 
 export default function App() {
   return (
     <>
       <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
-        <Router>
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/event" element={<CreateEvent />} />
-            <Route path="/event/:id" element={<Event />} />
-            <Route path="*" element={<Home />} />
-          </Routes>
-        </Router>
-        <Toaster />
+        <EventProvider>
+          <Router>
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/event" element={<CreateEvent />} />
+              <Route path="/event/:id" element={<Event />} />
+              <Route path="*" element={<Home />} />
+            </Routes>
+          </Router>
+          <Toaster />
+        </EventProvider>
       </ThemeProvider>
     </>
   )
--- front/src/hooks/use-event.ts
+++ front/src/hooks/use-event.ts
@@ -1,8 +1,18 @@
 import { FormEventValues } from '@/components/event-card'
+import { EventContext } from '@/contexts/event-context'
 import { api } from '@/utils/http/api'
 import { CreateEventResponse, Event } from '@/utils/types/event'
+import { useContext } from 'react'
 
 export const useEvent = () => {
+  const context = useContext(EventContext)
+
+  if (!context) {
+    throw new Error('useEvent must be used within an EventProvider')
+  }
+
+  const { isLogged, setIsLogged, paintedDivs, setPaintedDivs } = context
+
   const createEvent = async (
     event: FormEventValues
   ): Promise<CreateEventResponse> => {
@@ -24,6 +34,10 @@ export const useEvent = () => {
   }
 
   return {
-    createEvent
+    createEvent,
+    isLogged,
+    setIsLogged,
+    paintedDivs,
+    setPaintedDivs
   }
 }
