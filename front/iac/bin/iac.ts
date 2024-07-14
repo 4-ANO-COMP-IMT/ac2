ff5d4318f8b4efdd322ea3458fb9b48b1899a289
Vitor Soller <vgsoller@gmail.com>
2024-07-14 15:36:18 -0300

    feat(iac-front): add front iac with bucket and cdn😎

--- front/iac/bin/iac.ts
+++ front/iac/bin/iac.ts
@@ -1,21 +1,26 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { IacStack } from '../lib/iac-stack';
+import 'source-map-support/register'
+import * as cdk from 'aws-cdk-lib'
+import { IacStack } from '../lib/iac-stack'
 
-const app = new cdk.App();
-new IacStack(app, 'IacStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
+const app = new cdk.App()
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+const env = {
+  account: process.env.AWS_ACCOUNT_ID,
+  region: process.env.AWS_REGION
+}
 
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+const stackName = process.env.STACK_NAME || 'Ac2StackFrontdev'
+const projectName = process.env.PROJECT_NAME || 'Ac2Front'
 
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-});
+const tags = {
+  project: projectName,
+  stage: 'TEST',
+  stack: 'FRONT',
+  owner: 'AC2'
+}
+
+new IacStack(app, stackName, {
+  env: env,
+  tags: tags
+})
--- front/iac/lib/iac-stack.ts
+++ front/iac/lib/iac-stack.ts
@@ -1,16 +1,98 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from 'aws-cdk-lib'
+import * as s3 from 'aws-cdk-lib/aws-s3'
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront'
+import * as iam from 'aws-cdk-lib/aws-iam'
+
+import { Construct } from 'constructs'
 
 export class IacStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    super(scope, id, props)
 
-    // The code that defines your stack goes here
+    const stage = process.env.GITHUB_REF_NAME || 'dev'
+    const projectName = process.env.PROJECT_NAME || 'ViteReactTemplateFront'
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'IacQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const s3Bucket = new s3.Bucket(this, projectName + 'Bucket' + stage, {
+      versioned: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      accessControl: s3.BucketAccessControl.PRIVATE,
+      autoDeleteObjects: true
+    })
+
+    const oac = new cloudfront.CfnOriginAccessControl(this, 'AOC', {
+      originAccessControlConfig: {
+        name: projectName + 'Bucket OAC' + stage,
+        originAccessControlOriginType: 's3',
+        signingBehavior: 'always',
+        signingProtocol: 'sigv4'
+      }
+    })
+
+    const cloudFrontWebDistribution = new cloudfront.CloudFrontWebDistribution(
+      this,
+      'CDN',
+      {
+        comment: projectName + 'Distribution ' + stage,
+        originConfigs: [
+          {
+            s3OriginSource: {
+              s3BucketSource: s3Bucket
+            },
+            behaviors: [
+              {
+                isDefaultBehavior: true,
+                allowedMethods: cloudfront.CloudFrontAllowedMethods.GET_HEAD,
+                compress: true,
+                cachedMethods:
+                  cloudfront.CloudFrontAllowedCachedMethods.GET_HEAD,
+                viewerProtocolPolicy:
+                  cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+                minTtl: cdk.Duration.seconds(0),
+                maxTtl: cdk.Duration.seconds(86400),
+                defaultTtl: cdk.Duration.seconds(3600)
+              }
+            ]
+          }
+        ],
+        errorConfigurations: [
+          {
+            errorCode: 403,
+            responseCode: 200,
+            responsePagePath: '/index.html',
+            errorCachingMinTtl: 0
+          }
+        ]
+      }
+    )
+
+    const cfnDistribution = cloudFrontWebDistribution.node
+      .defaultChild as cloudfront.CfnDistribution
+
+    cfnDistribution.addPropertyOverride(
+      'DistributionConfig.Origins.0.OriginAccessControlId',
+      oac.getAtt('Id')
+    )
+
+    s3Bucket.addToResourcePolicy(
+      new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        actions: ['s3:GetObject'],
+        principals: [new iam.ServicePrincipal('cloudfront.amazonaws.com')],
+        resources: [s3Bucket.arnForObjects('*')]
+      })
+    )
+
+    new cdk.CfnOutput(this, projectName + 'BucketName-' + stage, {
+      value: s3Bucket.bucketName
+    })
+
+    new cdk.CfnOutput(this, projectName + 'DistributionId-' + stage, {
+      value: cloudFrontWebDistribution.distributionId
+    })
+
+    new cdk.CfnOutput(this, projectName + 'DistributionDomainName-' + stage, {
+      value: cloudFrontWebDistribution.distributionDomainName
+    })
   }
 }
