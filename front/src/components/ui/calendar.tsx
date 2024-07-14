315114e92182dd0f203bdb0d5eac4945f81ffab6
Vitor Soller <vgsoller@gmail.com>
2024-07-14 15:45:17 -0300

    fix(components): remove unsed props and imports

--- front/src/components/ui/calendar.tsx
+++ front/src/components/ui/calendar.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react'
-import { ChevronLeft, ChevronRight } from 'lucide-react'
 import { DayPicker } from 'react-day-picker'
 
 import { cn } from '@/lib/utils'
@@ -52,8 +51,6 @@ function Calendar({
         ...classNames
       }}
       components={{
-        IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
-        IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />
       }}
       {...props}
     />
