15d9e7a76577e604c7a73f610375c9985ea3eda5
Joao Branco <21.01075-7@maua.br>
2024-07-13 21:11:15 -0300

    feat(create_member): finish route with ThunderClient test

--- back/src/member/index.ts
+++ back/src/member/index.ts
@@ -2,16 +2,16 @@ import cors from 'cors'
 import express from 'express'
 
 import { environments } from '../shared/env/environments'
-import { eventRouter } from './routes/member.routes'
+import { memberRouter } from './routes/member.routes'
 
 const server = async () => {
-  const PORT = environments.eventPort
+  const PORT = environments.memberPort
 
   const app = express()
   app.use(express.json())
   app.use(cors())
 
-  app.use('/event', eventRouter)
+  app.use('/member', memberRouter)
 
   app.get('/', (req, res) => {
     res.send('API is running! 🦍 🚀 SIDIS')
