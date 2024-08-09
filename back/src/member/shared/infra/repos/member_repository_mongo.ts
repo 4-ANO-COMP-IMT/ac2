9d4602f1c5c7eec188525ba2a7d2066ef82ed6a6
Pedro Matumoto <pedromatumoto@gmail.com>
2024-08-09 17:57:23 -0300

    feat(event): add get event and create member

--- back/src/member/shared/infra/repos/member_repository_mongo.ts
+++ back/src/member/shared/infra/repos/member_repository_mongo.ts
@@ -5,6 +5,7 @@ import { Event } from '../../../../shared/domain/entities/event'
 import { Member } from '../../../../shared/domain/entities/member'
 import { environments } from '../../../../shared/env/environments'
 import { MemberRepositoryInterface } from './member_repository_interface'
+import { Availability } from '../../../../shared/domain/entities/availability'
 
 export class MemberRepositoryMongo implements MemberRepositoryInterface {
   mongoDBUser: string = environments.mongoDBUser
@@ -49,15 +50,78 @@ export class MemberRepositoryMongo implements MemberRepositoryInterface {
     }
   }
 
-  createMember(
+  async createMember(
     eventId: string,
     name: string,
     password?: string | undefined
   ): Promise<Member> {
-    throw new Error('Method not implemented.')
+    try {
+      await this.getEvent(eventId)
+    } catch (error) {
+      throw new Error('Event not found for eventId: ' + eventId)
+    }
+
+    const memberId: string = uuid()
+
+    const member = {
+      id: memberId,
+      name,
+      password
+    }
+
+    try {
+      await this.collection.updateOne(
+        { id: eventId },
+        { $push: { members: member } }
+      )
+    } catch (error) {
+      throw new Error('Error creating member')
+    }
+
+    return new Member(memberId, name, [], password)
   }
-  getEvent(eventId: string): Promise<Event> {
-    throw new Error('Method not implemented.')
+  async getEvent(eventId: string): Promise<Event> {
+    try {
+      const event = await this.collection.findOne({ id: eventId })
+
+      console.log('event:', event)
+
+      return new Event(
+        event.id,
+        event.name,
+        event.dates,
+        event.notEarlier,
+        event.notLater,
+        event.members
+          ? event.members.map(
+              (member: {
+                id: string
+                name: string
+                availabilities: any[]
+                password: string | undefined
+              }) =>
+                new Member(
+                  member.id,
+                  member.name,
+                  member.availabilities
+                    ? member.availabilities.map(
+                        (availability) =>
+                          new Availability(
+                            availability.id,
+                            availability.startDate,
+                            availability.endDate
+                          )
+                      )
+                    : [],
+                  member.password
+                )
+            )
+          : [],
+        event.description
+      )
+    } catch (error) {
+      throw new Error('Event not found for eventId: ' + eventId)
+    }
   }
   getMemberByName(name: string, eventId: string): Promise<Member | null> {
     throw new Error('Method not implemented.')
--- back/tests/member/shared/infra/repos/member_repository_mongo.test.ts
+++ back/tests/member/shared/infra/repos/member_repository_mongo.test.ts
@@ -2,7 +2,7 @@ import { describe, expect, it, test } from 'vitest'
 
 import { MemberRepositoryMongo } from '../../../../../src/member/shared/infra/repos/member_repository_mongo'
 
-describe('TestCreate event', () => {
+describe.skip('TestCreate event', () => {
   it('Create event', async () => {
     const repo = new MemberRepositoryMongo()
 
@@ -19,5 +19,61 @@ describe('TestCreate event', () => {
     expect(event.name).toBe('Event 1')
     expect(event.dates.length).toBe(2)
     expect(event.dates[0]).toBe(1616434800000)
+  })
+})
+
+describe.skip('TestCreate member', () => {
+  it('Create member', async () => {
+    const repo = new MemberRepositoryMongo()
+
+    const member = await repo.createMember(
+      '9b2f4e8c-8d59-11eb-8dcd-0242ac130003',
+      'Member 1',
+      'password'
+    )
+
+    expect(member.name).toBe('Member 1')
+    expect(member.password).toBe('password')
+  })
+
+  it('Event not found', async () => {
+    const repo = new MemberRepositoryMongo()
+
+    try {
+      await repo.createMember(
+        '9b2f4e8c-8d59-11eb-8dcd-0242ac130003',
+        'Member 1',
+        'password'
+      )
+    } catch (error) {
+      expect(error.message).toBe(
+        'Event not found for eventId: 9b2f4e8c-8d59-11eb-8dcd-0242ac130003'
+      )
+    }
+  })
+})
+
+describe.skip('TestGet event', () => {
+  it('Get event', async () => {
+    const repo = new MemberRepositoryMongo()
+
+    const event = await repo.getEvent('9b2f4e8c-8d59-11eb-8dcd-0242ac130003')
+
+    expect(event.id).toBe('9b2f4e8c-8d59-11eb-8dcd-0242ac130003')
+    expect(event.name).toBe('Event 1')
+    expect(event.dates.length).toBe(2)
+    expect(event.dates[0]).toBe(1616434800000)
+  })
+
+  it('Event not found', async () => {
+    const repo = new MemberRepositoryMongo()
+
+    try {
+      await repo.getEvent('9b2f4e8c-8d59-11eb-8dcd-0242ac130003')
+    } catch (error) {
+      expect(error.message).toBe(
+        'Event not found for eventId: 9b2f4e8c-8d59-11eb-8dcd-0242ac130003'
+      )
+    }
   })
 })
