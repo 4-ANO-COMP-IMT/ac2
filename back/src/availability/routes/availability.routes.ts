de2fe6be07d4efe981ab45cd2aebbda09a38ae53
Joao Branco <21.01075-7@maua.br>
2024-09-01 22:49:33 -0300

    refat(get_best_availabilities): change body param to query param

--- back/src/availability/routes/availability.routes.ts
+++ back/src/availability/routes/availability.routes.ts
@@ -4,6 +4,7 @@ import { HttpRequest } from '../../shared/domain/helpers/http/http_request'
 import { CommunicationHandler } from '../modules/communication/communication_handler'
 import { UpdateAvailabilitiesPresenter } from '../modules/update_availabilities/update_availabilities_presenter'
 import { GetBestAvailabilitiesPresenter } from '../modules/get_best_availabilities/get_best_availabilities_presenter'
+import { GetBestAvailabilitiesRequest } from '../modules/get_best_availabilities/protocols'
 
 export const availabilityRouter = Router()
 
@@ -17,10 +18,9 @@ availabilityRouter.put('/', async (req, res) => {
     .json({ message: response.message, data: response.data })
 })
 
-availabilityRouter.get('/get_best_availability', async (req, res) => {
-  const request = new HttpRequest('get', req.body)
+availabilityRouter.get('/get_best_availabilities', async (req, res) => { 
+  const request = new HttpRequest('get', req.query as GetBestAvailabilitiesRequest)
   const handler = new GetBestAvailabilitiesPresenter()
-  console.log('Got a request to /get_best_availability!')
   const response = await handler.call(request)
 
   res
