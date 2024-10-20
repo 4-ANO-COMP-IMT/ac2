9808ab6a1e6bd0ac5bf886bc383e356a9aae96b6
Enzo Sakamoto <enzo.sak@hotmail.com>
2024-10-20 20:16:06 -0300

    feat: add page router

--- flutter_app/lib/src/app.dart
+++ flutter_app/lib/src/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app/src/screens/home.dart';
+import 'package:flutter_app/src/pages/create_event_page.dart';
+import 'package:flutter_app/src/pages/home_page.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -7,26 +8,11 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          leading: const Icon(Icons.menu),
-          actions: [
-            Padding(
-              padding: const EdgeInsets.only(right: 24),
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  foregroundColor: Colors.black,
-                  backgroundColor: Colors.white,
-                  textStyle: const TextStyle(fontSize: 16),
-                ),
-                onPressed: () {},
-                child: const Text('Criar evento'),
-              ),
-            ),
-          ],
-        ),
-        body: const Home(),
-      ),
+      initialRoute: '/',
+      routes: {
+        '/': (_) => const HomePage(),
+        '/create-event': (_) => const CreateEventPage(),
+      },
     );
   }
 }
